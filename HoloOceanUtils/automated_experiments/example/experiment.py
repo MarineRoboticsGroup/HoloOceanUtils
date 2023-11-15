91bf406d767cd10541d85864bd98d6838a5f4d3b
Jungseok Hong <ssjason.hong@gmail.com>
2023-11-15 15:16:27 -0500

    additional json file added and updated experiment script

--- HoloOceanUtils/automated_experiments/example/experiment.py
+++ HoloOceanUtils/automated_experiments/example/experiment.py
@@ -3,10 +3,10 @@ from py_factor_graph.io.pyfg_text import read_from_pyfg_text
 
 
 if __name__ == '__main__':
-    run_experiment("HoloOceanUtils/automated_experiments/example/experiment_1/experiment_1.json")
-    pyfg = read_from_pyfg_text("HoloOceanUtils/automated_experiments/example/experiment_1/test_0.pyfg")
+    # run_experiment("/home/jungseok/git/HoloOceanUtils/HoloOceanUtils/automated_experiments/example/experiment_1/experiment_1.json")
+    pyfg = read_from_pyfg_text("/home/jungseok/git/HoloOceanUtils/HoloOceanUtils/automated_experiments/example/experiment_1/test_0.pyfg")
     pyfg.animate_odometry_3d(show_gt=True, draw_range_lines=True, num_timesteps_keep_ranges = 1)
-    pyfg = read_from_pyfg_text("HoloOceanUtils/automated_experiments/example/experiment_1/test_1.pyfg")
+    pyfg = read_from_pyfg_text("/home/jungseok/git/HoloOceanUtils/HoloOceanUtils/automated_experiments/example/experiment_1/test_1.pyfg")
     pyfg.animate_odometry_3d(show_gt=True, draw_range_lines=True, num_timesteps_keep_ranges = 1)
-    pyfg = read_from_pyfg_text("HoloOceanUtils/automated_experiments/example/experiment_1/test_2.pyfg")
+    pyfg = read_from_pyfg_text("/home/jungseok/git/HoloOceanUtils/HoloOceanUtils/automated_experiments/example/experiment_1/test_2.pyfg")
     pyfg.animate_odometry_3d(show_gt=True, draw_range_lines=True, num_timesteps_keep_ranges = 1)
